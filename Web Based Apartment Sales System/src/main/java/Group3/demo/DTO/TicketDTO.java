0fbc198f6b52614ebdb8b6a018912aa5a9ca76eb
IT24101174 <it24101174@my.sliit.lk>
2025-10-01 14:57:11 +0530

    Defined TicketDTO for data transfer with ticket details.

---
+++ Web Based Apartment Sales System/src/main/java/Group3/demo/DTO/TicketDTO.java
@@ -0,0 +1,49 @@
+package Group3.demo.DTO;
+
+public class TicketDTO {
+    private Integer id;
+    private Integer customerId;
+    private String subject;
+    private String description;
+    private String status;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Integer customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
