1fb2eeb63bbc89b475f1c06da1c3c55974522d05
P.W.I.H.Pahalawaththage <IT24101303@my.sliit.lk>
2025-10-23 02:42:39 +0530

    Delete Web Based Apartment Sales System/src/main/java/Group3/demo/Repository directory

--- Web Based Apartment Sales System/src/main/java/Group3/demo/Repository/InteractionRepository.java
+++
@@ -1,9 +0,0 @@
-package Group3.demo.Repository;
-
-import Group3.demo.Entity.Interaction;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface InteractionRepository extends JpaRepository<Interaction, Long> {
-    List<Interaction> findByLead_LeadId(Long leadId);
-}
